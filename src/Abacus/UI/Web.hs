99d57182ef6505c4707ce79d8d597ecce81654f0
Sarah Marshall <marshallsaraha@gmail.com>
2019-08-11 16:12:07 -0700

    Add basic input/output for Reflex calculator

--- src/Abacus/UI/Web.hs
+++ src/Abacus/UI/Web.hs
@@ -1,8 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Abacus.UI.Web (main) where
+module Abacus.UI.Web
+    ( main
+    )
+where
 
+import Abacus.Interpreter
+import Abacus.Utils
+import Control.Monad
+import Data.Text
 import Reflex.Dom
 
 main :: IO ()
-main = mainWidget $ el "div" $ text "Welcome to Reflex"
+main = mainWidget $ el "div" $ do
+    input <- textInput def
+    clicked <- button "="
+    let entered = void $ ffilter (isKey Enter) (_textInput_keypress input)
+    let submitted = evalString defaultEnv . unpack <$> tagPromptlyDyn
+            (_textInput_value input)
+            (clicked <> entered)
+    let result = pack . maybe "" showFloat . snd <$> filterRight submitted
+    holdDyn "" result >>= dynText
+    where isKey key code = key == keyCodeLookup (fromIntegral code)
