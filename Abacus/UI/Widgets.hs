a523b03be262992e6e2294b9e84feb358bdb011a
Sarah Marshall <marshallsaraha@gmail.com>
2019-08-16 00:44:28 -0700

    Show input text along with result value

--- Abacus/UI/Widgets.hs
+++ Abacus/UI/Widgets.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE TupleSections #-}
 
 module Abacus.UI.Widgets
     ( abacus
@@ -9,30 +10,41 @@ where
 
 import Abacus.Core.Interpreter
 import Abacus.Core.Utils
+import Data.Either.Combinators
 import Data.Text hiding (reverse)
-import Reflex.Dom
+import Reflex.Dom hiding (Input)
 import qualified Abacus.UI.History as History
+
+newtype Input = Input Text
+
+newtype Output = Output (Environment, Maybe Double)
+
+type SubmitResult = Either InterpretError (Input, Output)
 
 abacus :: MonadWidget t m => m ()
 abacus = el "div" $ mdo
     errorBox submitted
-    valueList submitted
+    resultList submitted
     submitted <- inputBox
     return ()
 
-errorBox :: MonadWidget t m => Event t InterpretResult -> m ()
+errorBox :: MonadWidget t m => Event t SubmitResult -> m ()
 errorBox submitted =
     let errorResult = pack . either show (const "") <$> submitted
     in  el "div" $ holdDyn "" errorResult >>= dynText
 
-valueList :: MonadWidget t m => Event t InterpretResult -> m ()
-valueList submitted = el "ul" $ do
-    let valueResult = pack . maybe "" showFloat . snd <$> filterRight submitted
-    values <- foldDyn (:) [] valueResult
-    _ <- simpleList (reverse <$> values) $ el "li" . dynText
+resultList :: MonadWidget t m => Event t SubmitResult -> m ()
+resultList submitted = el "dl" $ do
+    results <- foldDyn (:) [] $ filterRight submitted
+    _ <- simpleList (reverse <$> results) $ \result -> do
+        el "dt" $ dynText $ input <$> result
+        el "dd" $ dynText $ output <$> result
     return ()
+  where
+    input (Input i, _) = i
+    output (_, Output (_, o)) = pack $ maybe "" showFloat o
 
-inputBox :: MonadWidget t m => m (Event t InterpretResult)
+inputBox :: MonadWidget t m => m (Event t SubmitResult)
 inputBox = el "div" $ mdo
     input <- textInput def
         { _textInputConfig_setValue = difference
@@ -40,7 +52,7 @@ inputBox = el "div" $ mdo
             (_textInput_input input)
         }
     clicked <- button "="
-    let submitted = evalString defaultEnv . unpack <$> tagPromptlyDyn
+    let submitted = eval <$> tagPromptlyDyn
             (_textInput_value input)
             (clicked <> keypress Enter input)
     historyChanged <- accum (&) (History.singleton "") $ leftmost
@@ -50,3 +62,5 @@ inputBox = el "div" $ mdo
         , History.forward <$ keydown ArrowDown input
         ]
     return submitted
+  where
+    eval i = mapRight ((Input i,) . Output) $ evalString defaultEnv $ unpack i
